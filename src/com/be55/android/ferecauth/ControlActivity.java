ca4a46a0a9712c016133d73fcc2a876db1f969ee
YoKamaru <ka.ka.maru55+git@gmail.com>
2011-02-08 12:43:37 +0900

    First commit.

---
+++ src/com/be55/android/ferecauth/ControlActivity.java
@@ -0,0 +1,13 @@
+package com.be55.android.ferecauth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ControlActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
